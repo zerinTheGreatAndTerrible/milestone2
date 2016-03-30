80b10a4567a3e3af73566c3cbacf4a8a8cfa156e
zerinTheGreatAndTerrible <zerinbates@gmail.com>
2016-03-30 12:44:18 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,5 @@
 var express = require('express');
 var router = express.Router();
-
-var entries = [{slug:"what did I learn today",body:"that I am awesome"},{slug:"da da da",body:"dobedobedobe"}]
-/* GET til listing. */
-router.get('/', function(req, res, next) {
-  res.render('todayILearned', { title: 'Today I Learned', entries:entries});
-});
-/* the create database router*/
-router.get('/add', function(req, res, next) {
-  console.log(req.body);
-  entries.push(req.body);
-  console.log(entries);
-  res.render('add', { title: 'build data here', entries:entries});
-});
-/*when you hit submit*/
-router.post('/til/create', function(req, res, next) {
-  console.log(req.body);
-  entries.push(req.body);
-  console.log(entries);
-  res.render('todayILearned', { title: 'Today I Learned', entries:entries});
-});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Today I Learned' });
