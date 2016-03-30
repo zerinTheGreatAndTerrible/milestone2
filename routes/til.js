3249b247b11874a7f1685aac71934e73c2dcc56b
zerinTheGreatAndTerrible <zerinbates@gmail.com>
2016-03-30 12:59:22 -0600

    Update til.js

--- routes/til.js
+++ routes/til.js
@@ -7,16 +7,10 @@ router.get('/', function(req, res, next) {
 });
 /* the create database router*/
 router.get('/add', function(req, res, next) {
-  console.log(req.body);
-  entries.push(req.body);
-  console.log(entries);
   res.render('til/add', { title: 'build data here', entries:entries});
 });
 /*when you hit submit*/
 router.post('/til/create', function(req, res, next) {
-  console.log(req.body);
-  entries.push(req.body);
-  console.log(entries);
   res.render('todayILearned', { title: 'Today I Learned', entries:entries});
 });
 module.exports = router;
