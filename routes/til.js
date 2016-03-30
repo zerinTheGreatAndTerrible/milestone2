c30c148a1fe10529e082cad5072055cac88d76f2
zerinTheGreatAndTerrible <zerinbates@gmail.com>
2016-03-29 18:47:57 -0600

    Update til.js

--- routes/til.js
+++ routes/til.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var entries = [{slug:"what did I learn today",response:"that I am awesome"}]
 /* GET til listing. */
 router.get('/', function(req, res, next) {
-  res.render('todayILearned', { title: 'Today I Learned', entries:todayILearned});
+  res.render('todayILearned', { title: 'Today I Learned', todayILearned:entries});
 });
 
 module.exports = router;
