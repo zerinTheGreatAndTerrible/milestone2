7510a2bef30f0e061d61e079373937976dedb0d1
zerinTheGreatAndTerrible <zerinbates@gmail.com>
2016-03-29 19:31:52 -0600

    Update til.js

--- routes/til.js
+++ routes/til.js
@@ -5,5 +5,12 @@ var entries = [{slug:"what did I learn today",response:"that I am awesome"}]
 router.get('/', function(req, res, next) {
   res.render('todayILearned', { title: 'Today I Learned', entries:entries});
 });
+/* the create database router*/
+router.get('/', function(req, res, next) {
+  console.log(req.body);
+  entries.push(req.body);
+  console.log(entries);
+  res.render('CreateData', { title: 'build data here', entries:entries});
+});
 
 module.exports = router;
