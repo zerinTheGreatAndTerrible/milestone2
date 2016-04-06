ac07cfdf36ef14ec4965f59524a593244011221c
zerinTheGreatAndTerrible <zerinbates@gmail.com>
2016-04-06 12:34:19 -0600

    Update til.js

--- routes/til.js
+++ routes/til.js
@@ -1,17 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var entries = [{slug:"what did I learn today",body:"that I am awesome"},{slug:"da da da",body:"dobedobedobe"}]
-/* GET til listing. */
+/* READ all: GET entries listing. */
 router.get('/', function(req, res, next) {
-  req.db.driver.execQuery(
-    "SELECT*FROM entries;",
-    function(err,data){
-      if(err){
-        console.log(err);
-      }
-  res.render('todayILearned', { title: 'Today I Learned', entries:data});
-    }
-};
+ req.db.driver.execQuery(
+ "SELECT * FROM entries",
+ function (err, data) {
+ if(err){console.log(err);}
+ console.log(data);
+ res.render('todayILearned', { title: 'Today I Learned', entries: data });
+ }
+)
 });
 /* the create database router*/
 router.get('/add', function(req, res, next) {
