a9bc2d0963b4d92a0a5ae144544b0fb382adc025
zerinTheGreatAndTerrible <zerinbates@gmail.com>
2016-05-05 12:40:48 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,5 @@ var entries = [{slug:"what did I learn today",body:"that I am awesome"},{slug:"d
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Today I Learned' });
 });
-router.get('/til/',function(req,res,next)){
- rew.render('todayILearned',{title: 'today I learned',entries:entries})
-});
+
 module.exports = router;
