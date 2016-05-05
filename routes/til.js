75d72e5d803e265d228420e1b535826b50fabe8c
zerinTheGreatAndTerrible <zerinbates@gmail.com>
2016-05-05 12:30:39 -0600

    Update til.js

--- routes/til.js
+++ routes/til.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var entries = [{slug:"what did I learn today",body:"that I am awesome"},{slug:"da da da",body:"dobedobedobe"}]
+var entries = [{slug:"what did I learn today",body:"that I am awesome"},{slug:"da da da",body:"dobedobedobe"}];
 router.get('/',function(req,res,next)){
  rew.render('todayILearned',{title: 'today I learned',entries:entries})
 });
